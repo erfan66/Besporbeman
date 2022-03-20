91c6cc3576e79d15a04f528245ffd7c05dca77f0
erfan66 <erfan.azizifard@gmail.com>
2022-03-20 23:20:46 +0300

    I initialized DbSet in ApplicationDbContext.

--- BMDataAccess/Data/ApplicationDbContext.cs
+++ BMDataAccess/Data/ApplicationDbContext.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using BMModel.Areas;
+using BMModel.Categories;
+using BMModel.Categories.Types;
+using BMModel.Model;
+using BMModel.Personals;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,7 +16,26 @@ namespace BMDataAccess.Data
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
-
+         
         }
+        public DbSet<City> City { get; set; }
+        public DbSet<Country> Country { get; set; }
+        public DbSet<Doc> Doc { get; set; }
+        public DbSet<Electronic> Electronic { get; set; }
+        public DbSet<NonElectronic> NonElectronic { get; set; }
+        public DbSet<Kind> Kind { get; set; }
+
+        public DbSet<Material> Material { get; set; }
+        public DbSet<Receiver> Receiver { get; set; }
+
+        public DbSet<Sender> Sender { get; set; }
+        public DbSet<Transfer> Transfer { get; set; }
+
+
+
+
+
+
+
     }
 }
