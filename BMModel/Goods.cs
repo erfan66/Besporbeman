e3a1b068d79e24f5c2dc8c3cad898ece366c770d
erfan66 <erfan.azizifard@gmail.com>
2022-03-26 14:09:50 +0300

    *Add Goods Model

---
+++ BMModel/Goods.cs
@@ -0,0 +1,25 @@
+﻿using BMModel.Categories;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMModel
+{
+    public class Goods
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int KindId { get; set; }
+        [ForeignKey("KindId")]
+        public Kind Kind { get; set; }
+        [Required]
+        public int MaterialId { get; set; }
+        [ForeignKey("MaterialId")]
+        public Material Material { get; set; }
+    }
+}
