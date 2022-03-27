b355391befee2e63e474ba2533f7be3379b6fac8
erfan66 <erfan.azizifard@gmail.com>
2022-03-27 16:30:13 +0300

    *Add Category folder/index(Empty) *Add Repository folder/Repository(full) *Add IRepository folder/IRepository(full)

---
+++ BMDataAccess/Repository/Repository.cs
@@ -0,0 +1,77 @@
+﻿using BMDataAccess.Data;
+using BMDataAccess.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMDataAccess.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _db;
+        internal DbSet<T> dbset;
+        public Repository(ApplicationDbContext db)
+        {
+            _db = db;
+            dbset = _db.Set<T>();
+        }
+        public void Add(T entity)
+        {
+            dbset.Add(entity);
+        }
+
+        public IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter = null, Func<IQueryable<T>, IOrderedQueryable<T>>? orderby = null, string? includeProperties = null)
+        {
+            IQueryable<T> query = dbset;
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+            if (includeProperties!=null)
+            {
+                foreach (var includeProperty in includeProperties.Split(
+                    new char[] { ','},StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProperty);
+                }
+            }
+            if (orderby!=null)
+            {
+                return orderby(query).ToList();
+            }
+            return query.ToList();
+        }
+
+        public T GetFirstOrDefault(Expression<Func<T, bool>>? filter = null, string? includeProperties = null)
+        {
+            IQueryable<T> query = dbset;
+            if (filter!=null)
+            {
+                query = query.Where(filter);
+            }
+            if (includeProperties!=null)
+            {
+                foreach (var includeProperty in includeProperties.Split(
+                    new char[] {','},StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProperty);
+                }
+            }
+            return query.FirstOrDefault();
+        }
+
+        public void Remove(T entity)
+        {
+            dbset.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            dbset.RemoveRange(entities);
+        }
+    }
+}
