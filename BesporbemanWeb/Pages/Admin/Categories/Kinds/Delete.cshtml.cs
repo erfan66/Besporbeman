f08e298df0309e3bab27fcb54344ed41769cae34
erfan66 <erfan.azizifard@gmail.com>
2022-03-31 14:19:14 +0300

    *Make UI & PageModel for Create,Edit and Delete in the Kind *Add  _Notification in the shared folder and configure toastr in  the _Layout *Change Theme

--- BesporbemanWeb/Pages/Admin/Categories/Kinds/Delete.cshtml.cs
+++ BesporbemanWeb/Pages/Admin/Categories/Kinds/Delete.cshtml.cs
@@ -1,12 +1,33 @@
+using BMDataAccess.Repository.IRepository;
+using BMModel.Categories;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace BesporbemanWeb.Pages.Admin.Categories.Kinds
 {
+    [BindProperties]
     public class DeleteModel : PageModel
     {
-        public void OnGet()
+        private readonly IUnitOfWork _unitOfWork;
+        public Kind Kind { get; set; }
+        public DeleteModel(IUnitOfWork unitOfWork)
         {
+            _unitOfWork = unitOfWork;
+        }
+        public void OnGet(int id)
+        {
+            Kind = _unitOfWork.Kind.GetFirstOrDefault(x => x.Id == id);
+        }
+        public async Task<IActionResult> OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Kind.Remove(Kind);
+                _unitOfWork.Save();
+                TempData["success"] = "Kind Deleted Successfully";
+                return RedirectToPage("Index");
+            }
+            return Page();
         }
     }
 }
--- BesporbemanWeb/Pages/Admin/Categories/Kinds/Edit.cshtml.cs
+++ BesporbemanWeb/Pages/Admin/Categories/Kinds/Edit.cshtml.cs
@@ -1,12 +1,34 @@
+using BMDataAccess.Repository.IRepository;
+using BMModel.Categories;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace BesporbemanWeb.Pages.Admin.Categories.Kinds
 {
+    [BindProperties]
     public class EditModel : PageModel
     {
-        public void OnGet()
+        private readonly IUnitOfWork _unitOfWork;
+        public Kind Kind { get; set; }
+        public EditModel(IUnitOfWork unitOfWork)
         {
+            _unitOfWork = unitOfWork;
+        }
+        public void OnGet(int id)
+        {
+            Kind = _unitOfWork.Kind.GetFirstOrDefault(x => x.Id == id);
+        }
+        public async Task<IActionResult> OnPost()
+        {
+
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Kind.Update(Kind);
+                _unitOfWork.Save();
+                TempData["success"] = "Kind Updated Successfully!";
+                return RedirectToPage("Index");
+            }
+            return Page();
         }
     }
 }
