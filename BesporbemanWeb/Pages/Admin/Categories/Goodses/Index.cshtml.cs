7859abd8ec01d324f06cc876d92b0b661d55570e
erfan66 <erfan.azizifard@gmail.com>
2022-04-02 16:01:36 +0300

    *Make UI Index of the Goodses folder *Make empty goodses.js file

--- BesporbemanWeb/Pages/Admin/Categories/Goodses/Index.cshtml.cs
+++ BesporbemanWeb/Pages/Admin/Categories/Goodses/Index.cshtml.cs
@@ -7,15 +7,9 @@ namespace BesporbemanWeb.Pages.Admin.Categories.Goodses
 {
     public class IndexModel : PageModel
     {
-        private readonly IUnitOfWork _unitOfWork;
-        public IEnumerable<Goods> AllGoods { get; set; }
-        public IndexModel(IUnitOfWork unitOfWork)
-        {
-            _unitOfWork = unitOfWork;
-        }
         public void OnGet()
         {
-            AllGoods = _unitOfWork.Goods.GetAll(includeProperties: "Kind,Material");
+
         }
     }
 }
