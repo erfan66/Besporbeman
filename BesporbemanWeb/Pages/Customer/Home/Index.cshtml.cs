39236e14b248d15f93d9502b048a7a154ede63ad
erfan66 <erfan.azizifard@gmail.com>
2022-04-11 00:44:29 +0300

    *I madeHome and Card folder *I made Index razor page in the Home folder *I made some properties in the UI and Page Model of the Index

---
+++ BesporbemanWeb/Pages/Customer/Home/Index.cshtml.cs
@@ -0,0 +1,24 @@
+using BMDataAccess.Repository.IRepository;
+using BMModel;
+using BMModel.Categories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BesporbemanWeb.Pages.Customer.Home
+{
+    public class IndexModel : PageModel
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public IEnumerable<Advertise> AdvertiseList { get; set; }
+        public IEnumerable<Kind> KindList { get; set; }
+        public IndexModel(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public void OnGet()
+        {
+            AdvertiseList = _unitOfWork.Advertise.GetAll(includeProperties: "Kind,Material");
+            KindList = _unitOfWork.Kind.GetAll();
+        }
+    }
+}
